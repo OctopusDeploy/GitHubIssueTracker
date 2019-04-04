5e2f27b30efb2bba0d1df0032d86fbf4966111e1
Henrik Andersson <henrik.andersson@octopus.com>
2019-04-05 07:21:42 +1000

    Retrieve release note from GitHub

--- source/Server/WorkItems/WorkItemLinkMapper.cs
+++ source/Server/WorkItems/WorkItemLinkMapper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Octokit;
 using Octopus.Server.Extensibility.Extensions.WorkItems;
 using Octopus.Server.Extensibility.HostServices.Model.PackageMetadata;
 using Octopus.Server.Extensibility.IssueTracker.GitHub.Configuration;
@@ -10,12 +12,17 @@ namespace Octopus.Server.Extensibility.IssueTracker.GitHub.WorkItems
     {
         private readonly IGitHubConfigurationStore store;
         private readonly CommentParser commentParser;
+        private readonly IGitHubClient githubClient;
+        private readonly Regex ownerRepoRegex = new Regex("(?:https?://)?(?:[^?/\\s]+[?/])(.*)", RegexOptions.Compiled);
+
 
         public WorkItemLinkMapper(IGitHubConfigurationStore store,
-            CommentParser commentParser)
+            CommentParser commentParser,
+            IGitHubClient githubClient)
         {
             this.store = store;
             this.commentParser = commentParser;
+            this.githubClient = githubClient;
         }
 
         public string CommentParser => GitHubConfigurationStore.CommentParser;
@@ -32,16 +39,36 @@ namespace Octopus.Server.Extensibility.IssueTracker.GitHub.WorkItems
 
             var isEnabled = store.GetIsEnabled();
 
+            var releaseNotePrefix = store.GetReleaseNotePrefix();
             var workItemReferences = commentParser.ParseWorkItemReferences(packageMetadata);
 
             return workItemReferences.Select(wir => new WorkItemLink
                 {
                     Id = wir.IssueNumber,
-                    Description = wir.IssueNumber,
+                    Description = isEnabled ? GetReleaseNote(packageMetadata.VcsRoot, wir.IssueNumber, releaseNotePrefix) : wir.IssueNumber,
                     LinkUrl = isEnabled ? NormalizeLinkData(baseUrl, packageMetadata.VcsRoot, wir.LinkData) : wir.LinkData
                 })
                 .Distinct()
                 .ToArray();
+        }
+
+        public string GetReleaseNote(string vcsRoot, string issueNumber, string releaseNotePrefix)
+        {
+            if (string.IsNullOrWhiteSpace(releaseNotePrefix))
+                return issueNumber;
+
+            var ownerRepoParts = ownerRepoRegex.Match(vcsRoot).Groups[1]?.Value.Split('/');
+            if (ownerRepoParts.Count() < 2)
+                return issueNumber;
+
+            var releaseNoteRegex = new Regex($"^{releaseNotePrefix}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var issueComments = githubClient.Issue.Comment.GetAllForIssue(ownerRepoParts[0], ownerRepoParts[1], int.Parse(issueNumber)).Result;
+
+            var releaseNote = issueComments?.LastOrDefault(c => releaseNoteRegex.IsMatch(c.Body))?.Body;
+            if (!string.IsNullOrWhiteSpace(releaseNote))
+                return releaseNoteRegex.Replace(releaseNote, "")?.Trim();
+
+            return issueNumber;
         }
 
         public static string NormalizeLinkData(string baseUrl, string vcsRoot, string linkData)
