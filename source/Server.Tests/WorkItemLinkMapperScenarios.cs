f3b6dec4dd0d17da618ac29c87caf52fe2de87ee
Shane <tothegills@gmail.com>
2020-07-06 14:49:21 +1000

    Support SSH root URLs when generating work item links
    Relates to OctopusDeploy/Issues#6467

--- source/Server.Tests/WorkItemLinkMapperScenarios.cs
+++ source/Server.Tests/WorkItemLinkMapperScenarios.cs
@@ -55,6 +55,7 @@ namespace Octopus.Server.Extensibility.IssueTracker.GitHub.Tests
         [TestCase("https://github.com", "https://github.com/UserX/RepoY", "UserX/RepoZ#1234", ExpectedResult = "https://github.com/UserX/RepoZ/issues/1234")]
         [TestCase("https://github.com", "https://github.com/UserX/RepoY", "https://github.com/UserX/RepoY/issues/1234", ExpectedResult = "https://github.com/UserX/RepoY/issues/1234")]
         [TestCase("https://github.com", "", "UserX/RepoZ#1234", ExpectedResult = "https://github.com/UserX/RepoZ/issues/1234")]
+        [TestCase("https://github.com", "git@github.com:UserX/RepoY", "#1234", ExpectedResult = "https://github.com/UserX/RepoY/issues/1234")]
         public string NormalizeLinkData(string baseUrl, string vcsRoot, string linkData)
         {
             return WorkItemLinkMapper.NormalizeLinkData(baseUrl, vcsRoot, linkData);
